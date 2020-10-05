7a46aad8c7e00e1a6e7d7bfba15695d88b7ab39d
chandan-2ly <chandansah625@gmail.com>
2020-10-06 00:55:23 +0530

    dev done for who we serve

--- src/component/HomeComponent/OurClients/clientCard.js
+++ src/component/HomeComponent/OurClients/clientCard.js
@@ -5,7 +5,8 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import Typography from '@material-ui/core/Typography';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
@@ -30,22 +31,18 @@ const useStyles = makeStyles((theme) => ({
   cover: {
     width: 550,
   },
-  controls: {
-    display: 'flex',
-    alignItems: 'center',
-    paddingLeft: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
+  baseCover: {
+    width: 80,
+    height: 80
   },
-  playIcon: {
-    height: 38,
-    width: 38,
+  button: {
+    margin: theme.spacing(1),
   },
 }));
 
 export default function ClientCard({data}) {
   const classes = useStyles();
   const theme = useTheme();
-  const [clientInfo, setClientInfo] = React.useState([]);
 
   return (
     <Card className={classes.root}>
@@ -62,22 +59,18 @@ export default function ClientCard({data}) {
             </Grid>
         </Paper>
         </CardContent>
-        <div className={classes.controls}>
-          <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon className={classes.playIcon} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
-        </div>
+        <Grid container direction="row" justify="space-evenly" alignItems="center">
+          <CardMedia className={classes.baseCover} image={data.baseImage1}  title="client motto" />
+          <CardMedia className={classes.baseCover} image={data.baseImage2}  title="client motto" />
+          <Button variant="contained" color="primary" className={classes.button} endIcon={<ArrowForwardIcon/>}>
+            Know More
+          </Button>
+        </Grid>
       </div>
       <CardMedia
         className={classes.cover}
         image={data.image}
-        title="Live from space album cover"
+        title="who we serve"
       />
     </Card>
   );
