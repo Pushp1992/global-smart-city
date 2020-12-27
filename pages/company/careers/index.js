c66793827dbdff8284853f54fd33dd6120d29d7d
pushp-1992 <72811558+pushp-1992@users.noreply.github.com>
2020-12-27 16:33:12 +0530

    dev done for company career pages

--- pages/company/careers/index.js
+++ pages/company/careers/index.js
@@ -1,9 +1,190 @@
-import React from 'react';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Paper from "@material-ui/core/Paper";
 
-const AboutUs = () => {
-    return(
-        <h2>careers page</h2>
-    )
-}
+import Box from "@material-ui/core/Box";
 
-export default AboutUs;
+import NavBar from "../../../src/component/nav-bar";
+import Footer from "../../../src/component/Footer/footer";
+import styles from "../../../styles/home.module.scss";
+
+import { CompanyCareer } from "../../../src/utils/MenuItems/about-company/careers";
+
+const style = {
+  topMargin: {
+    marginTop: "40px",
+  },
+  padding: {
+    padding: "15px",
+  },
+  headingTextColor: {
+    color: "teal",
+    fontWeight: "600 !important",
+  },
+  buttonMargin: {
+    margin: "40px",
+  },
+  progressBarStyle: {
+    backgroundColor: "black",
+  },
+  buttonStyle: {
+    backgroundColor: "teal",
+    opacity: "0.8",
+    width: "15rem",
+    height: "4rem",
+    fontSize: "20px",
+    fontWeight: "700",
+  },
+  applyText: {
+    fontWeight: "bold",
+    marginTop: "30px",
+  },
+  cardHeading: {
+    color: "teal",
+    fontSize: "2rem",
+  },
+  cardSubHeading: {
+    fontFamily: "serif",
+  },
+  card: {
+    cover: {
+      width: "20rem",
+      height: "20rem",
+      marginTop: "20px",
+    },
+    root: {
+      display: "inline-flex",
+    },
+  },
+};
+
+const CoverCard = () => {
+  return (
+    <Container disableGutters={true} maxWidth="false">
+      <Grid item md>
+        <Card className={styles.coverCardStyle}>
+          <CardHeader
+            title={CompanyCareer.bannerHeading}
+            style={style.headingTextColor}
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+              <label>{CompanyCareer.bannerSubHeading}</label>
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Container>
+  );
+};
+
+const OurGoal = () => {
+  return (
+    <React.Fragment>
+      <Container disableGutters={true} maxWidth="false">
+        <Grid item md className={styles.navBarPosition}>
+          <NavBar />
+        </Grid>
+
+        <Grid item md style={style.topMargin}>
+          <CoverCard />
+        </Grid>
+
+        <Grid item md style={style.topMargin}>
+          <Typography
+            align="left"
+            display="initial"
+            variant="h5"
+            style={style.headingTextColor}
+          >
+            Our Goals
+          </Typography>
+        </Grid>
+
+        <Grid item md style={style.topMargin}>
+          <img
+            src={CompanyCareer.bannerImage}
+            alt="cover_image"
+            //style={{ width: "-webkit-fill-available" }}
+            className={styles.fullScreenWidth}
+          />
+        </Grid>
+
+        <Grid item md style={style.topMargin}>
+          <div style={style.padding}>
+            <Typography>{CompanyCareer.mainContent}</Typography>
+          </div>
+        </Grid>
+      </Container>
+
+      <Container
+        disableGutters={false}
+        maxWidth="false"
+        style={style.topMargin}
+        className="test me"
+      >
+        <Grid item md align="center">
+          {CompanyCareer.cards.map((item, index) => {
+            return (
+              <Grid key={index} item md={6} style={style.topMargin}>
+                <Card className={styles.careerCard}>
+                  <div style={style.card.media}>
+                    <CardMedia
+                      style={style.card.cover}
+                      image={item.cardImage}
+                      title={item.cardHeading}
+                    />
+                  </div>
+
+                  <div>
+                    <CardContent style={style.topMargin}>
+                      <Typography style={style.cardHeading}>
+                        {item.cardHeading}
+                      </Typography>
+                      <Typography style={style.topMargin}>
+                        <label style={style.cardSubHeading}>
+                          {item.cardSubHeading}
+                        </label>
+                      </Typography>
+                    </CardContent>
+                  </div>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+      <Container disableGutters={true} maxWidth="true" style={style.topMargin}>
+        <Grid item md style={style.topMargin} align="center">
+          <LinearProgress style={style.progressBarStyle} />
+          <div style={style.buttonMargin}>
+            <Button variant="contained" style={style.buttonStyle}>
+              {" "}
+              Apply Now
+            </Button>
+            <Typography style={style.applyText}>
+              Search Roles, Apply or Continue an Application
+            </Typography>
+          </div>
+          <LinearProgress style={style.progressBarStyle} />
+        </Grid>
+      </Container>
+
+      <Container disableGutters={true} maxWidth="false" style={style.topMargin}>
+        <Grid item md>
+          <Footer />
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default OurGoal;
---
+++ src/utils/MenuItems/about-company/careers.js
@@ -0,0 +1,40 @@
+const BASE_URL =
+  "https://res.cloudinary.com/pushpcloud/image/upload/v1608458732/smart-city/MenuBar/AboutCompany/careers";
+
+const CompanyCareer = {
+  bannerImage: `${BASE_URL}/Careers-1_qeivjk`,
+  bannerHeading: "Join SDP",
+  bannerSubHeading: "Help us to make our cities & villages more Liveable",
+  mainContent: `Few organizations want to change the world. We're beginning 
+    by improving this most needed steps by transforming lives in cities & villages.
+    We are enthusiastic tech-business experts devoted to pushing government 
+    authorities to carry out transformative approach for citizens.`,
+  cards: [
+    {
+      cardImage: `${BASE_URL}/Careers-2_xkpfib`,
+      cardHeading: "SDP Purpose",
+      cardSubHeading: `We are determined to provide pragmatic, comprehensive, technologically 
+            advanced and innovative strategic solutions to achieve results that bridge what is with 
+            what can be. We provide advisory and consultancy services in the social, economic, 
+            Institutional and physical development of an advanced and inclusive society.`,
+    },
+    {
+      cardImage: `${BASE_URL}/Careers-3_m9aieb`,
+      cardHeading: "SDP Life",
+      cardSubHeading: `Working at SDP means uphold, motivation, adaptability and endless occasions
+             to develop and learn. We are committed to welcoming all individuals that have a richness of 
+             viewpoints and a keen ability to challenge established thinking and maintaining the healthy work-life balance.`,
+    },
+    {
+      cardImage: `${BASE_URL}/Careers-4_ir5tcv`,
+      cardHeading: "Roles at SDP",
+      cardSubHeading: `We offer multitude of Senior, middle and young expert roles at SDP, look for a wide 
+            scope of abilities from digital to relational to logical so as to work with the most persuasive 
+            associations on the planet, helping them contend in the present relentless climate and manufacture 
+            their future. The work we do each day diving deep to find key experiences and having the mental 
+            fortitude to kick off something new prompts an enduring effect on organizations, enterprises, and society.`,
+    },
+  ],
+};
+
+export { CompanyCareer };
