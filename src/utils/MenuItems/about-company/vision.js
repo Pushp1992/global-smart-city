a3f39e1e647098d4457ef67553171386c5766fdb
Pushp Singh <pushp.ranjansingh@gmail.com>
2020-12-28 19:39:35 +0530

    Integration (#6)
    * added pages for About-Company and Our-Goals
    
    * dev done for company career pages
    
    * dev done for team page
    
    * dev done for VMV pages
    
    Co-authored-by: pushp-1992 <72811558+pushp-1992@users.noreply.github.com>

---
+++ src/utils/MenuItems/about-company/vision.js
@@ -0,0 +1,77 @@
+const BASE_URL =
+  "https://res.cloudinary.com/pushpcloud/image/upload/v1608458732/smart-city/MenuBar/AboutCompany/vision";
+
+const CompanyVMV = {
+  headerImage: `${BASE_URL}/vmv-1_ko0uly`,
+  topBanner: {
+    bannerImage: `${BASE_URL}/vmv-2_vlqyzd`,
+    bannerHeading: "Nurturing our purposes & Values",
+    bannerSubHeading: "Codes of Conduct",
+    content: ``,
+  },
+  bottomBanner: {
+    bannerImage: `${BASE_URL}/vmv-4_msz3wa`,
+    bannerHeading: "Diversity & Inclusion",
+    bannerSubHeading: "Diversity: the art of thinking independently together",
+    content: `At SDP, we regard and worth contrasts. We realize that when individuals from various
+    foundations and with various perspectives cooperate, we make the most worth – for our customers, 
+    our kin and society. When we listen and celebrate what is both common and different, we become wiser,
+    more inclusive, and better as an organization.`,
+  },
+  cards: [
+    {
+      tagLine: "Our Vision",
+      details: `To be a leading organization providing path breaking solutions fostering smart communities across the globe.`,
+    },
+    {
+      tagLine: "Our Mission",
+      details: `To facilitate best living conditions by developing cities, regions and infrastructure that are proudly inherited by coming generations.`,
+    },
+    {
+      tagLine: "Our Values",
+      details:
+        "Quality Excellence,Innovative Eminence,Socially Responsible,Collaborative Culture",
+    },
+  ],
+  ceoSection: {
+    avatar: `${BASE_URL}/vmv-3_ojiyzw`,
+    name: "SD Prakash,",
+    designation: "Founder & CEO",
+    company: "SDP LLC",
+    subject: `Opening message from CEO & Founder`,
+    salutation: "Dear Colleagues,",
+    messageBody: [
+      {
+        paragraph: `SDP is perceived as a worldwide innovator in proficient administrations,
+        working with world's most notable associations in nations around the globe. As we 
+        observe get greater and more complex challenges and opportunities , we keep on adjusting.`,
+      },
+      {
+        paragraph: `With over 200+ of us working across in excess of 83 nations, we each have a task 
+        to carry out in having a genuine effect for our customers, the capital business sectors, our groups,
+        our networks, and our more extensive partners. We have an obligation to understand why we do the 
+        things we do through our Purpose, what we centre around through our system, and how we cooperate 
+        through our qualities, practices, and lead.`,
+      },
+      {
+        paragraph: `From ecological impressions to social effects on financial specialist requests and 
+        everything in the middle of, organizations are relied upon to show responsive and dependable 
+        conduct in more regions and be responsible to an ever more extensive exhibit of partners.`,
+      },
+      {
+        paragraph: `The trust that our clients, communities and our people place in SDP, and our high 
+        standards of ethical behaviour, are fundamental to everything we do. As we go about our work it’s 
+        important we have a frame of reference for the decisions we make every day. Our Code of Conduct (Code)
+        can guide us, no matter where we are or what we do.  It's how we do business.`,
+      },
+      {
+        paragraph: `The Code strengthens the significance of directing business inside the structure of
+        expert principles, laws, and guidelines, along with our own strategies, qualities, and norms,
+        even as we work across fringes. It traces the qualities and practices that characterize
+        how we work together. It considers us responsible to be receptive and responsive and to put forth a valiant effort.`,
+      },
+    ],
+  },
+};
+
+export { CompanyVMV };
