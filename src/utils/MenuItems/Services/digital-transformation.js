a8c02a587fc706093859b56e64e265b9ff26f321
Pushp Singh <pushp.ranjansingh@gmail.com>
2021-02-07 22:44:43 +0530

    dev done for digital transformation (#15)
    dev done for digital transformation

---
+++ src/utils/MenuItems/Services/digital-transformation.js
@@ -0,0 +1,46 @@
+const BASE_URL =
+  "https://res.cloudinary.com/pushpcloud/image/upload/v1608458732/smart-city/MenuBar/Services/digital-transformation";
+
+const DigitalTransformation = {
+  bannerImage: `${BASE_URL}/transformation_xzgg9k`,
+  firstSection: {
+    title: "Digital Transformation",
+    description: `Going under transformation to digital is the first step,
+    we can track and analyse the data that has been captured during the
+    services which have been availed. Using data-driven insights can help 
+    to understand customers better and also rethink business strategies,
+    assisting with better decision-making, paving ways to a higher ROI.`,
+  },
+  secondSection: {
+    title: "What we do?",
+    description: `At SDP, we understand, identify the area which requires transformation 
+    in terms of digital services. We provide the best in-fit strategy & solution as per our client’s requirement.`,
+  },
+  thirdSection: {
+    title: "What we Offer?",
+    description: [
+      { id: 1, title: `Digital Transformation Strategy` },
+      { id: 2, title: `Immersion and acceleration labs` },
+      { id: 3, title: `Emerging technology visioning and prototyping` },
+      { id: 4, title: `Disruptive business models` },
+      { id: 5, title: `Digital operating model` },
+      { id: 6, title: `Emerging technology-driven strategy` },
+      { id: 7, title: `Performance optimization` },
+      { id: 8, title: `Journey mapping` },
+      { id: 9, title: `Digital enterprise and system architecture` },
+      { id: 10, title: `Digital Solutions` },
+      { id: 11, title: `Cyber and information security` },
+      { id: 12, title: `Cloud transformation` },
+      {
+        id: 13,
+        title: `Artificial Intelligence (AI) based Predictive analytics`,
+      },
+      { id: 14, title: `NLP and machine learning` },
+      { id: 15, title: `Analytics and big data platforms` },
+      { id: 16, title: `Digital program design and management` },
+      { id: 17, title: `Digital investment performance management` },
+    ],
+  },
+};
+
+export { DigitalTransformation };
