a01dd7f86ea21240df8a7d260454e9c4aa2e1d21
pushp-1992 <72811558+pushp-1992@users.noreply.github.com>
2021-02-07 21:27:07 +0530

    dev done for govt advisory service

---
+++ src/utils/MenuItems/Services/advisory.js
@@ -0,0 +1,44 @@
+const BASE_URL =
+  "https://res.cloudinary.com/pushpcloud/image/upload/v1608458732/smart-city/MenuBar/Services/advisory";
+
+const Advisory = {
+  bannerImage: `${BASE_URL}/advisory_sr6dcg`,
+  firstSection: {
+    title: "Government Advisory",
+    description: `National & Local Government Advisory services are the foundation for the 
+    citizens to enhance quality of life of the citizens. We as a team with diverse geographical
+    and technical experience use technology and smarter approaches to advise better services all around the world.`,
+  },
+  secondSection: {
+    title: "What we do?",
+    description: `At SDP, we provide government advisory services to local, national, 
+    regional and global government entities As per the mandates of several ministries, 
+    whether Ministry of Urban Development, Rural Development, Economy, Education, 
+    Communication & IT and Health etc. we support in every verticals.`
+  },
+  thirdSection: {
+    title: "What we Offer?",
+    description: [
+      { id: 1, title: `Programme Performance Monitoring` },
+      { id: 2, title: `Capacity Building & Knowledge Management` },
+      { id: 3, title: `Vendor & Technology Selection` },
+      { id: 4, title: `Stakeholders Management` },
+      { id: 5, title: `City Performance Indicators (KPIs)` },
+      { id: 6, title: `Tender RFP/RFQ/RFI Preparation` },
+      { id: 7, title: `Urban Policies & Smart Governance` },
+      { id: 8, title: `FDI for Smart Cities (Market Entries)` },
+      { id: 9, title: `Service Delivery, Partnership & Strategy` },
+      { id: 10, title: `United Nations SDGs Alignment` },
+      { id: 11, title: `Smart Business Engagement Models` },
+      { id: 12, title: `Innovation & Urban Living Lab` },
+      { id: 13, title: `Emerging Technologies Strategy` },
+      { id: 14, title: `Tech & Business Proposal Writing` },
+      { id: 15, title: `Budget Management` },
+      { id: 16, title: `Pricing Analysis of Projects` },
+      { id: 17, title: `Government Liaoning` },
+      { id: 18, title: `Resilient Cities Development` },
+    ],
+  },
+};
+
+export { Advisory };
