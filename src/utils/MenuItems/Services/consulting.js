ed027c54d72d4a15618ac2b2eb36c8036248ae89
pushp-1992 <72811558+pushp-1992@users.noreply.github.com>
2021-02-07 21:58:41 +0530

    dev for for consulting service

---
+++ src/utils/MenuItems/Services/consulting.js
@@ -0,0 +1,48 @@
+const BASE_URL =
+  "https://res.cloudinary.com/pushpcloud/image/upload/v1608458732/smart-city/MenuBar/Services/consulting";
+
+const Consulting = {
+  bannerImage: `${BASE_URL}/consulting_ibhdn3`,
+  firstSection: {
+    title: "Management Consulting",
+    description: `In management consulting, we provide you expert solutions in particular
+    areas such as smart infrastructure development projects, business development, 
+    smart education, smart health care, smart infrastructures and in many other specialized
+    fields. During the learning solution design and development phase, the required learning 
+    outcomes, assessment and learning strategies are determined. This phase also determines
+    the most effective delivery method to ensure the most effective learning solution is
+    designed and development to meet the needs of all learners.`,
+  },
+  secondSection: {
+    title: "What we do?",
+    description: `At SDP, we bring deep, functional expertise, but are known for our holistic
+    perspective: we capture value across boundaries and between the silos of any organization.
+    We have proven a multiplier effect from optimizing the sum of the parts, not just the
+    individual pieces. Our consulting services focus on our clients' most critical issues and opportunities`,
+  },
+  thirdSection: {
+    title: "What we Offer?",
+    description: [
+      { id: 1, title: `Program and Project Management consulting` },
+      { id: 2, title: `Strategy Consulting-Technology & Business` },
+      { id: 3, title: `Transformation roadmap` },
+      { id: 4, title: `Innovation Management` },
+      { id: 5, title: `Smart Technologies Solutions` },
+      { id: 6, title: `Designing Smart Solutions` },
+      { id: 7, title: `Solution Architecture` },
+      { id: 8, title: `Developing smart solutions` },
+      { id: 9, title: `Solutions Integration Strategy` },
+      { id: 10, title: `Master System Integration` },
+      { id: 11, title: `Solutions Use cases` },
+      { id: 12, title: `Operational Consulting` },
+      { id: 13, title: `Industry Competitive Analysis ` },
+      { id: 14, title: `Market & Research Analysis` },
+      { id: 15, title: `Business Plans & consulting` },
+      { id: 16, title: `E-commerce Management` },
+      { id: 17, title: `Financial Consulting` },
+      { id: 18, title: `Market Strategy` },
+    ],
+  },
+};
+
+export { Consulting };
